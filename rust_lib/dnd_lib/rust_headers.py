1013d2c51bd9b03a9d2aa5339a2729ce161acf0b
Kotleth <wtablecie97@gmail.com>
2023-11-17 11:28:24 +0100

    [Pathfinding] Implemented dijkstra algorithm to calucalate the minimal distance between squares

--- rust_lib/dnd_lib/rust_headers.py
+++ rust_lib/dnd_lib/rust_headers.py
@@ -3,7 +3,7 @@ from os import getcwd
 import numpy as np
 new_path = getcwd()
 # my_lib = ctypes.CDLL(f'{new_path}/target/release/libtesting.dylib')
-my_lib = ctypes.CDLL(f'{new_path}/target/debug/libdnd_lib.dylib')
+my_lib = ctypes.CDLL(f'{new_path}/target/release/libdnd_lib.dylib')
 
 
 '''
@@ -11,19 +11,26 @@ Rust Wrapper
 '''
 
 
-def testing_func(mat_a):
-    my_lib.testing_function.argtypes = [np.ctypeslib.ndpointer(dtype=np.float32), ctypes.c_int, ctypes.c_int]
-    my_lib.testing_function.restype = ctypes.POINTER(ctypes.c_float)
+def dijkstra_distances(mat_a, starting_point):
+    my_lib.dijkstra.argtypes = [np.ctypeslib.ndpointer(dtype=np.float32), ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
+    my_lib.dijkstra.restype = ctypes.POINTER(ctypes.c_int)
 
     a_matrix = np.array(mat_a, dtype=np.float32)
-    print(np.size(a_matrix))
-    print(int(np.sqrt(len(a_matrix))))
-    result_ptr = my_lib.testing_function(a_matrix, np.size(a_matrix), len(a_matrix))
-    result_array = np.ctypeslib.as_array(result_ptr, shape=(np.size(a_matrix),))
-    x = result_array.copy()
-    my_lib.free(result_ptr)
-    return x
+
+    # I am not really sure how to get anything more than a pointer from Rust to Python without dealing with Cython etc.
+    # So this parser needs to stay like this for now
+    result_ptr_1 = my_lib.dijkstra(a_matrix, np.size(a_matrix), len(a_matrix), starting_point[0], starting_point[1])
+    result_array_1 = np.ctypeslib.as_array(result_ptr_1, shape=(np.size(a_matrix) * 3,))
+    distances_array = result_array_1.copy()
+    my_lib.free(result_ptr_1)
+    distances_dict = {}
+    for i in range(int(len(distances_array) / 3)):
+        distances_dict[(distances_array[i*2], distances_array[i*2 + 1])] = distances_array[i + int(len(distances_array) * 2 / 3)]
+    return distances_dict
 
 
-matrix = [[2.0, 3.0, 5.0, 6.0], [1.0, 4.0, 6.0, 9.0], [7.0, 8.0, 9.0, 11.0], [3.0, -4.0, 7.0, 13.0]]
-print(testing_func(matrix))
+matrix = [[50, 100, 50, 50], [50, 50, 50, 50]]
+# matrix = [[50.0, 100.0, 50.0, 50.0], [50.0, 50.0, 50.0, 50.0]]
+start = (0, 0)
+print(testing_func(matrix, start))
+
--- rust_lib/dnd_lib/src/lib.rs
+++ rust_lib/dnd_lib/src/lib.rs
@@ -1,32 +1,88 @@
+use std::collections::{BinaryHeap, HashMap, HashSet};
+// use std::cmp::{Ordering, Ord, PartialOrd};
+use std::cmp::Ordering;
 
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-//
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+#[derive(Eq, PartialEq)]
+struct Node {
+    name: (usize, usize),
+    distance: u32,
+}
 
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.distance.cmp(&self.distance)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 #[no_mangle]
-pub fn testing_function(matrix: &[f32], num_rows: usize) -> (*const f32, usize){
-    let a_matrix = make_matrix(matrix, num_rows, num_rows).unwrap();
-    let mut temp_vec: Vec<f32> = Vec::new();
-    for row in &a_matrix{
-        for i in row{
-            temp_vec.push(*i);
+// pub fn dijkstra(in_matrix: &[f32], rows: usize, x_start: usize, y_start: usize) -> (*const f32, usize)
+pub fn dijkstra(in_matrix: &[f32], rows: usize, x_start: usize, y_start: usize) -> (*const i32, usize)
+{
+    // let init_matrix = in_matrix.iter().map(|&x| x as u32).collect();
+    let init_matrix: Vec<u32> = in_matrix.clone().iter().map(|&x| x as u32).collect();
+    let start: (usize, usize) = (x_start, y_start);
+    let mut init_graph: HashMap<(usize, usize), HashMap<(usize, usize), u32>> = HashMap::new();
+    let cols: usize = init_matrix.len() / rows;
+    for i in 0..init_matrix.len() {
+        init_graph.insert((i % rows, (i - i % rows) / rows), HashMap::new());
+        for j in [-1, 1] {
+            // These are for top and bottom boundaries
+            if ((i % rows != 0) | (j == 1)) & ((i % rows != rows - 1) | (j == -1)) {
+                let neighbour_row: usize = ((i % rows) as i32 + j) as usize;
+                init_graph.get_mut(&(i % rows, (i - i % rows) / rows)).map(|val| val.insert((neighbour_row, (i - i % rows) / rows), init_matrix[(i as i32 + j) as usize]));
+            }
+            // These are for left and right boundaries
+            if (((i - i % rows) / rows != 0) | (j == 1)) & (((i - i % rows) / rows != cols - 1) | (j == -1)) {
+                let neighbour_col: usize = (((i - i % rows) / rows) as i32 + j) as usize;
+                init_graph.get_mut(&(i % rows, (i - i % rows) / rows)).map(|val| val.insert((i % rows, neighbour_col), init_matrix[(i as i32 + j * rows as i32) as usize]));
+            }
         }
     }
-    let slice_ptr = temp_vec.as_slice().as_ptr();
-    let slice_len = temp_vec.len();
-    std::mem::forget(temp_vec);
-    (slice_ptr, slice_len)
+    let graph = init_graph.clone();
+    let mut distances: HashMap<(usize, usize), u32> = init_graph.keys().map(|&k| (k, u32::MAX)).collect();
+    let mut priority_queue = BinaryHeap::new();
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+
+    distances.insert(start, 0);
+    priority_queue.push(Node { name: start, distance: 0 });
+    while let Some(Node { name, distance }) = priority_queue.pop() {
+        if visited.contains(&name) {
+            continue;
+        }
+
+        visited.insert(name);
+
+        for (&neighbor, &weight) in graph[&name].iter() {
+            let new_distance = distance + weight;
+            if new_distance < *distances.get(&neighbor).unwrap() {
+                distances.insert(neighbor, new_distance);
+                priority_queue.push(Node { name: neighbor, distance: new_distance });
+            }
+        }
+    }
+
+    let mut temp_keys_vec = Vec::new();
+    let mut temp_values_vec = Vec::new();
+
+    // Make it a pointer to pass it to python
+    for (&keys, &values) in distances.iter() {
+        let (x, y) = keys;
+        temp_keys_vec.push(x as i32);
+        temp_keys_vec.push(y as i32);
+        temp_values_vec.push(values as i32);
+    }
+    temp_keys_vec.append(&mut temp_values_vec);
+    let merged_vec = temp_keys_vec;
+
+    let slice_pointer = merged_vec.as_slice().as_ptr();
+    let slice_len = merged_vec.len();
+    std::mem::forget(merged_vec);
+    (slice_pointer, slice_len)
 }
 
 fn make_matrix(vector: &[f32], num_rows: usize, num_columns: usize) -> Result<Vec<Vec<f32>>, String> {
@@ -45,5 +101,4 @@ fn make_matrix(vector: &[f32], num_rows: usize, num_columns: usize) -> Result<Ve
         Ok(res_mat)
     }
 }
-
 
